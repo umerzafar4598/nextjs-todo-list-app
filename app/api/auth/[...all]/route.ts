907771cbe803a3890634ee38a158af58214cf650
Umer Zafar <umerzafar4598@gmail.com>
2026-04-30 14:30:50 +0500

    Deployment Changes 02

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,5 @@
 import { auth } from "@/lib/auth"; // path to your auth file
 import { toNextJsHandler } from "better-auth/next-js";
+export const dynamic = "force-dynamic";
 
 export const { POST, GET } = toNextJsHandler(auth);
