3578a6e328576de4875f1fa2833a1b77f2d789d7
Umer Zafar <umerzafar4598@gmail.com>
2026-04-30 14:40:50 +0500

    Deployment Changes 03

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,7 @@
-import 'dotenv/config'
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema'
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "./schema";
 
+const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+export const db = drizzle(sql, { schema });
